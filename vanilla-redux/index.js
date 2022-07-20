1a88dc1e1c84c49f29b36d895fa2b5635d04a432
JaeyoonCheon <cjswodbs50@gmail.com>
2022-07-20 18:25:35 +0900

    vanila JS redux complete

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,14 +1,18 @@
+import { legacy_createStore as createStore } from "redux";
+
 const divToggle = document.querySelector(".toggle");
 const counter = document.querySelector("h1");
 const btnIncrease = document.querySelector("#increase");
 const btnDecrease = document.querySelector("#decrease");
+
+console.log(btnIncrease);
 
 const TOGGLE_SWITCH = "TOGGLE_SWITCH";
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
-const increase = () => ({ type: INCREASE });
+const increase = (difference) => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
 const initialState = {
@@ -37,3 +41,33 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  counter.innerText = state.counter;
+};
+
+render();
+
+store.subscribe(render);
+
+divToggle.addEventListener("click", () => {
+  console.log("toggle!");
+  store.dispatch(toggleSwitch());
+});
+btnIncrease.addEventListener("click", () => {
+  console.log("increase!");
+  store.dispatch(increase(1));
+});
+btnDecrease.addEventListener("click", () => {
+  console.log("decrease!");
+  store.dispatch(decrease());
+});
