f4d436e4179f16d3ecb23d2ad7798ea56c2de41f
JaeyoonCheon <cjswodbs50@gmail.com>
2022-07-26 16:07:35 +0900

    react code-splitting complete

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Suspense } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import loadable from "@loadable/component";
+
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>Loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} className="App-logo" alt="logo"></img>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {visible && <SplitMe></SplitMe>}
       </header>
     </div>
   );
