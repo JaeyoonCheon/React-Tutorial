ef497925cddc401ce5865dc22461c5b3a3b5383e
JaeyoonCheon <cjswodbs50@gmail.com>
2022-07-27 21:16:24 +0900

    Nodejs koa backend complete

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const koa = require("koa");
 const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
 
 const api = require("./api");
 
@@ -7,6 +8,8 @@ const app = new koa();
 const router = new Router();
 
 router.use("/api", api.routes());
+
+app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
