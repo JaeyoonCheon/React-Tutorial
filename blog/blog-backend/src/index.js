2ed76b0bc11ba5946217e4bffacdd8af1036090d
JaeyoonCheon <cjswodbs50@gmail.com>
2022-08-02 15:03:23 +0900

    Blog backend install koa-static

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -3,6 +3,10 @@ import koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import mongoose from "mongoose";
+import serve from "koa-static";
+import path from "path";
+import send from "koa-send";
+import * as url from "url";
 
 import api from "./api/index.js";
 import jwtMiddleware from "./lib/jwtMiddleware.js";
@@ -27,6 +31,15 @@ app.use(bodyParser());
 app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const buildDirectory = path.resolve(__dirname, "../../blog-frontend/build");
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf("/api") !== 0) {
+    await send(ctx, "index.html", { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
